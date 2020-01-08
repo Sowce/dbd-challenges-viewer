d8c93550baec063c0a30e7c92b9a3bcc322d6467
Sowce <lyoxchan@gmail.com>
2020-01-09 00:19:24 +0100

    adding completion & hide completed

--- src/App.js
+++ src/App.js
@@ -4,14 +4,56 @@ import FlipMove from "react-flip-move";
 import archivesLogo from "./images/archives.png";
 import "./App.css";
 
+const getLocalStorage = item => window.localStorage.getItem(item);
+const setLocalStorage = (item, value) =>
+  window.localStorage.setItem(
+    item,
+    typeof value === "object" ? JSON.stringify(value) : value
+  );
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state = { searchText: "" };
+    let challengeState = {};
+    let hideCompleted = false;
+
+    if (getLocalStorage("challengeState"))
+      challengeState = JSON.parse(getLocalStorage("challengeState"));
+    if (getLocalStorage("hideCompleted"))
+      hideCompleted = Boolean(getLocalStorage("hideCompleted"));
+
+    this.state = { searchText: "", challengeState, hideCompleted };
 
     this.onSearch = this.onSearch.bind(this);
+    this.onChallengeClick = this.onChallengeClick.bind(this);
     this.parseLevels = this.parseLevels.bind(this);
+    this.getChallengeState = this.getChallengeState.bind(this);
+    this.onHideCompleted = this.onHideCompleted.bind(this);
+  }
+
+  onChallengeClick(id) {
+    this.setState(
+      {
+        challengeState: {
+          ...this.state.challengeState,
+          [id]: !this.state.challengeState[id]
+        }
+      },
+      () => {
+        setLocalStorage("challengeState", this.state.challengeState);
+      }
+    );
+  }
+
+  onHideCompleted(event) {
+    this.setState(
+      { hideCompleted: event.target.checked },
+      setLocalStorage("hideCompleted", event.target.checked)
+    );
+  }
+
+  getChallengeState(id) {
+    return Boolean(this.state.challengeState[id]);
   }
 
   isMasterChallenge(challenge) {
@@ -34,7 +76,7 @@ class App extends React.Component {
   }
 
   parseLevels(levels) {
-    return Object.keys(levels)
+    let result = Object.keys(levels)
       .map(level =>
         this.parseLevel(levels[level]).map(challenge => ({
           ...challenge,
@@ -48,6 +90,13 @@ class App extends React.Component {
           .includes(this.state.searchText.toLowerCase())
       )
       .sort((a, b) => a.level - b.level);
+
+    if (this.state.hideCompleted)
+      result = result.filter(
+        challenge => !this.getChallengeState(challenge.id)
+      );
+
+    return result;
   }
 
   onSearch(e) {
@@ -61,19 +110,36 @@ class App extends React.Component {
           <img className="appLogo" src={archivesLogo} alt="" />
           <div className="appTitle">DBD Challenges</div>
           <div className="spacer"></div>
+          <div className="checkBoxContainer">
+            <input
+              type="checkbox"
+              name="hideCompleted"
+              checked={this.state.hideCompleted}
+              onChange={this.onHideCompleted}
+              id="hideCompleted"
+            />
+            <label htmlFor="hideCompleted">Hide Completed</label>
+          </div>
           <input type="text" placeholder="Search..." onChange={this.onSearch} />
         </div>
         <FlipMove className="content">
           {this.parseLevels(challenges.levels).map(challenge => {
-            const cardClassName = `challengeCard ${challenge.target} ${
-              this.isMasterChallenge(challenge) ? "master" : ""
-            }`;
+            const cardClassName = `challengeCard ${challenge.target}${
+              this.isMasterChallenge(challenge) ? " master" : ""
+            }${this.getChallengeState(challenge.id) ? " done" : ""}`;
 
             return (
-              <div className={cardClassName}>
+              <div
+                className={cardClassName}
+                onClick={() => this.onChallengeClick(challenge.id)}
+                key={challenge.id}
+              >
                 <div className="rewardsCol">
                   {challenge.rewards.map(reward => (
-                    <div className="rewardRow">
+                    <div
+                      className="rewardRow"
+                      key={`${reward.id}-${reward.amount}`}
+                    >
                       <img
                         className="rewardIcon"
                         src={`./${reward.id}.png`}
