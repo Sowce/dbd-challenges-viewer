d43e695886bda28f3eb72dbe81770df82e0506c5
Sowce <lyoxchan@gmail.com>
2020-01-08 21:52:11 +0100

    Updates

---
+++ precache-manifest.df4f1df09d413468f8ec3256c4af0900.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94010cc6afbad9852194a437e074f436",
+    "url": "/dbd-challenges-viewer/index.html"
+  },
+  {
+    "revision": "9af331fcd470c6ce8965",
+    "url": "/dbd-challenges-viewer/static/css/main.bbce9128.chunk.css"
+  },
+  {
+    "revision": "676392be60eca91d490f",
+    "url": "/dbd-challenges-viewer/static/js/2.f6f7d10f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dbd-challenges-viewer/static/js/2.f6f7d10f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9af331fcd470c6ce8965",
+    "url": "/dbd-challenges-viewer/static/js/main.951d95b9.chunk.js"
+  },
+  {
+    "revision": "e6d424adb2b6e49bbc43",
+    "url": "/dbd-challenges-viewer/static/js/runtime-main.974fb675.js"
+  },
+  {
+    "revision": "846cb884ec0282bac2d90abf58dc168c",
+    "url": "/dbd-challenges-viewer/static/media/archives.846cb884.png"
+  },
+  {
+    "revision": "8d966a800d04b3e00862d2a861982e84",
+    "url": "/dbd-challenges-viewer/static/media/survivor.8d966a80.png"
+  }
+]);
