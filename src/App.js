bf48c4a63e87548d18cb766c7794bbe71e6bc976
Sowce <lyoxchan@gmail.com>
2020-01-08 21:45:21 +0100

    first push, lots of cool things

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import challenges from "./challenges.json";
+import FlipMove from "react-flip-move";
+import archivesLogo from "./images/archives.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { searchText: "" };
+
+    this.onSearch = this.onSearch.bind(this);
+    this.parseLevels = this.parseLevels.bind(this);
+  }
+
+  isMasterChallenge(challenge) {
+    return (
+      challenge.rewards.find(reward => reward.id === "riftfragments").amount > 3
+    );
+  }
+
+  cleanObjective(objective) {
+    return (objective || "").replace(/<\/?b>/gi, "");
+  }
+
+  parseLevel(level) {
+    return Object.keys(level)
+      .filter(level => level !== "unlocks")
+      .map(category =>
+        level[category].map(challenge => ({ ...challenge, target: category }))
+      )
+      .flat();
+  }
+
+  parseLevels(levels) {
+    return Object.keys(levels)
+      .map(level =>
+        this.parseLevel(levels[level]).map(challenge => ({
+          ...challenge,
+          level
+        }))
+      )
+      .flat()
+      .filter(challenge =>
+        challenge.objective
+          .toLowerCase()
+          .includes(this.state.searchText.toLowerCase())
+      );
+  }
+
+  onSearch(e) {
+    this.setState({ searchText: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="appBar">
+          <img className="appLogo" src={archivesLogo} alt="" />
+          <div className="appTitle">DBD Challenges</div>
+          <div className="spacer"></div>
+          <input type="text" placeholder="Search..." onChange={this.onSearch} />
+        </div>
+        <FlipMove className="content">
+          {this.parseLevels(challenges.levels).map(challenge => {
+            const cardClassName = `challengeCard ${challenge.target} ${
+              this.isMasterChallenge(challenge) ? "master" : ""
+            }`;
+
+            return (
+              <div className={cardClassName}>
+                <div className="rewardsCol">
+                  {challenge.rewards.map(reward => (
+                    <div className="rewardRow">
+                      <img
+                        className="rewardIcon"
+                        src={`./${reward.id}.png`}
+                        alt={reward.id}
+                      />
+                      {reward.amount}
+                    </div>
+                  ))}
+                </div>
+                <div className="challengeCol">
+                  <div style={{ width: "100%" }}>
+                    Tome <b>2</b> - Level <b>{challenge.level}</b>
+                  </div>
+                  <div>{this.cleanObjective(challenge.objective)}</div>
+                </div>
+              </div>
+            );
+          })}
+        </FlipMove>
+      </div>
+    );
+  }
 }
 
 export default App;
